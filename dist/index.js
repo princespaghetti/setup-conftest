05944a899120e840ce030eb7be8efb09a7fe7982
Anthony Barbieri <anthonyabarbieri@gmail.com>
2022-11-16 19:23:29 -0500

    upgrade node types and rebuild dist

--- dist/index.js
+++ dist/index.js
@@ -114,25 +114,32 @@ function getVersion() {
     });
 }
 function getAllVersions() {
-    var e_1, _a;
+    var _a, e_1, _b, _c;
     return __awaiter(this, void 0, void 0, function* () {
         const githubToken = core.getInput('github-token', { required: true });
         const octokit = github.getOctokit(githubToken);
         const allVersions = [];
         try {
-            for (var _b = __asyncValues(octokit.paginate.iterator(octokit.rest.repos.listReleases, { owner: 'open-policy-agent', repo: 'conftest' })), _c; _c = yield _b.next(), !_c.done;) {
-                const response = _c.value;
-                for (const release of response.data) {
-                    if (release.name) {
-                        allVersions.push(release.name);
+            for (var _d = true, _e = __asyncValues(octokit.paginate.iterator(octokit.rest.repos.listReleases, { owner: 'open-policy-agent', repo: 'conftest' })), _f; _f = yield _e.next(), _a = _f.done, !_a;) {
+                _c = _f.value;
+                _d = false;
+                try {
+                    const response = _c;
+                    for (const release of response.data) {
+                        if (release.name) {
+                            allVersions.push(release.name);
+                        }
                     }
+                }
+                finally {
+                    _d = true;
                 }
             }
         }
         catch (e_1_1) { e_1 = { error: e_1_1 }; }
         finally {
             try {
-                if (_c && !_c.done && (_a = _b.return)) yield _a.call(_b);
+                if (!_d && !_a && (_b = _e.return)) yield _b.call(_e);
             }
             finally { if (e_1) throw e_1.error; }
         }
